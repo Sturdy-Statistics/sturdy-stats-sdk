e6129caa25710bc9ed77c66beaa77b082de5a143
Kian Ghodoussi <ghodoussikian@gmail.com>
2025-08-14 09:49:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sturdy-stats-sdk",
-    version="1.0.47",
+    version="1.0.46",
     author="Kian Ghodoussi",
     author_email="ghodoussikian@gmail.com",
     description="SDK for the Sturdy Statistics API",
